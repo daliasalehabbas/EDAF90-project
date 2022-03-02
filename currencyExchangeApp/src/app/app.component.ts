2b406380c8da228184cd645e1c16bfb3ffccab90
daliasalehabbas <daliasalehabbas@gmail.com>
2022-03-02 17:10:52 +0100

    Added method to fetch api:s and designed front page

--- currencyExchangeApp/src/app/app.component.ts
+++ currencyExchangeApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'currencyExchangeApp';
+  tryResp:any;
+
+  constructor(private httpClient: HttpClient){
+  }
+
+  method(url:string){
+  this.httpClient.get("http://api.exchangeratesapi.io/v1/latest?access_key=7ec58f1adbc4b7c0654f3d03687420c6" + url).subscribe(response =>{
+    this.tryResp=response
+    console.log(response)
+  })
+
+  }
+
+  
+  title = 'currenntExchangeApp';
 }
---
+++ currencyExchangeApp/src/app/exchangeside/exchangeside.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exchangeside',
+  templateUrl: './exchangeside.component.html',
+  styleUrls: ['./exchangeside.component.css']
+})
+export class ExchangesideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
