a0ea392d1fac198755b413c0703c24b78e55becc
Mohammed Khalil <mohammedkhalil@MacBook-Air-som-tillhor-Mohammed.local>
2022-03-04 16:46:51 +0100

    Har fixat en navbar och 3 links som länkar till Home, exchange och graph

--- currencyExchangeApp/src/app/app.module.ts
+++ currencyExchangeApp/src/app/app.module.ts
@@ -1,26 +1,32 @@
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExchangesideComponent } from './exchangeside/exchangeside.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GraphViewerComponent } from './graph-viewer/graph-viewer.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ExchangesideComponent,
     GraphViewerComponent,
-    NavBarComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
