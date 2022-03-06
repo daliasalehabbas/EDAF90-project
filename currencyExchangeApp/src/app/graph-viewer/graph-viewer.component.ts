bec094fec20291c697f5c13d8acc0402b7b21134
Daniel <danne09090@gmail.com>
2022-03-06 13:28:52 +0100

    Added input validation, added 'global'-files for potential use

--- currencyExchangeApp/src/app/graph-viewer/graph-viewer.component.ts
+++ currencyExchangeApp/src/app/graph-viewer/graph-viewer.component.ts
@@ -7,18 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graph-viewer.component.css']
 })
 export class GraphViewerComponent implements OnInit {
-  tryResp:any;
-  currencies:any;
-  startDate:any;
-  endDate:any;
-  base:any;
-  symbols:any;
+  tryResp: any;
+  currencies: any;
+  startDate: any;
+  endDate: any;
+  base: any;
+  symbols: any;
 
   constructor(private httpClient: HttpClient) {
     //Finns det smartare sätt att skriva ut alla valutor? Funderade på att hämta ut dem
     //m.h.a http, men ska man behöva skicka en get request varje gång man öppnar denna sidan?
     this.currencies = ['AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN',
-                       'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF'];
+      'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF'];
     this.startDate = "";
     this.endDate = "";
     this.base = "";
@@ -29,13 +29,19 @@ export class GraphViewerComponent implements OnInit {
 
   }
 
-  method(date:string, base:string, symbols:[]){
-    var symbString = symbols.join(',');
-    this.httpClient.get("http://api.exchangeratesapi.io/v1/"+date+"?access_key=7ec58f1adbc4b7c0654f3d03687420c6& base = "+base+"& symbols = "+symbString)
-    .subscribe((response: any) =>{
-      this.tryResp=response
-      console.log(response)
-    })
-    }
+  getHistoric(date: string, base: string, symbols: string) {
+    console.log(date)
+    console.log(base)
+    console.log(symbols)
+    //var symbString = symbols.join(',');
+    this.httpClient.get("http://api.exchangeratesapi.io/v1/" + date +
+      "?access_key=7ec58f1adbc4b7c0654f3d03687420c6" +
+      "&base=" + base +
+      "&symbols=" + symbols)
+      .subscribe((response: any) => {
+        this.tryResp = response
+        console.log(response)
+      })
+  }
 
 }
