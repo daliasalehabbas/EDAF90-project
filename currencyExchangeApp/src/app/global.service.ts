943659fdb42ea408a5d9cb4dd1924f45966fb60b
Mohammed Khalil <mohammedkhalil@MacBook-Air-som-tillhor-Mohammed.local>
2022-03-10 15:44:42 +0100

    HELA GRAFENgit add .

--- currencyExchangeApp/src/app/global.service.ts
+++ currencyExchangeApp/src/app/global.service.ts
@@ -7,15 +7,19 @@ import { delay, map, lastValueFrom } from 'rxjs';
   providedIn: 'root'
 })
 export class GlobalService {
-  __year: number;
+  one_year: number;
+  three_years:number;
+  five_years:number;
   __month: number;
   __week: number;
   __day: number;
   __extractedValue: any;
-  __historicValues:any;
+  __historicValues: any;
 
   constructor(private http: HttpClient) {
-    this.__year = 31536000;
+    this.one_year = 31536000;
+    this.three_years = 94694400;
+    this.five_years = 157766400;
     this.__month = 2678400;
     this.__week = 604800;
     this.__day = 86400;
@@ -32,69 +36,108 @@ export class GlobalService {
     return Number(new Date(YYYYMMDD).getTime() / 1000);
   }
 
-  async get_histroic(fromCurrency: string, toCoin: string, startDate: string, endDate?: string) {
+  async get_histroic(fromCurrency: string, toCoin: string, startDate: string, endDate: string) {
     this.__historicValues = [];
     let start_time_unix: number = this.convert_date_to_unix(startDate);
-    if (endDate) {
-      let end_time_unix: number = this.convert_date_to_unix(endDate);
-      let interval: number = end_time_unix - start_time_unix;
+    let end_time_unix: number = this.convert_date_to_unix(endDate);
+    let interval: number = end_time_unix - start_time_unix;
+    console.log(interval)
+    if (interval == this.__week) {
+      console.log("Week")
+      for (let i = start_time_unix; i < end_time_unix; i += this.__day) {
+        let value = this.get_from_date(fromCurrency, toCoin, i.toString())
+        await value.then(resp => {
+          this.__historicValues.push(Object.values(resp)[0][fromCurrency])
+        })
+      }
+    } else if (interval>=2419200 &&interval <= this.__month) {
+      console.log("Month")
+      for (let i = start_time_unix; i <= end_time_unix; i += 2 * this.__day) {
+        let value = this.get_from_date(fromCurrency, toCoin, i.toString())
+        await value.then(resp => {
+          this.__historicValues.push(Object.values(resp)[0][fromCurrency])
+        })
+      }
+    } else if (interval == this.one_year) {
+      console.log("year")
+      for (let i = start_time_unix; i <= end_time_unix; i += this.__month) {
+        let value = this.get_from_date(fromCurrency, toCoin, i.toString())
+        await value.then(resp => {
+          this.__historicValues.push(Object.values(resp)[0][fromCurrency])
+        })
+      }
+    } else if (interval == this.three_years) {
+      console.log("3 years")
+      for (let i = start_time_unix; i <= end_time_unix+this.__month; i += 4 * this.__month) {
+        let value = this.get_from_date(fromCurrency, toCoin, i.toString())
+        await value.then(resp => {
+          this.__historicValues.push(Object.values(resp)[0][fromCurrency])
+        })
+      }
+    } else if (interval == this.five_years) {
+      console.log("5 years")
+      for (let i = start_time_unix; i <= end_time_unix+this.one_year; i += 6 * this.__month) {
+        let value = this.get_from_date(fromCurrency, toCoin, i.toString())
+        await value.then(resp => {
+          this.__historicValues.push(Object.values(resp)[0][fromCurrency])
+        })
+      }
+    } else if (interval > this.five_years){
+      console.log('+5 years')
+      for (let i = start_time_unix; i <= end_time_unix+this.one_year; i +=  this.one_year) {
+        let value = this.get_from_date(fromCurrency, toCoin, i.toString())
+        await value.then(resp => {
+          this.__historicValues.push(Object.values(resp)[0][fromCurrency])
+        })
+      }
+    }
+     else {
+      console.log("this year")
       if (interval <= this.__week) {
-        console.log("Week")
         for (let i = start_time_unix; i <= end_time_unix; i += this.__day) {
           let value = this.get_from_date(fromCurrency, toCoin, i.toString())
           await value.then(resp => {
             this.__historicValues.push(Object.values(resp)[0][fromCurrency])
           })
         }
-      } else if (interval > this.__week && interval <= this.__month) {
-        console.log("Month")
+      } else if (this.__week < interval && interval <= this.__month) {
         for (let i = start_time_unix; i <= end_time_unix; i += 2 * this.__day) {
           let value = this.get_from_date(fromCurrency, toCoin, i.toString())
           await value.then(resp => {
             this.__historicValues.push(Object.values(resp)[0][fromCurrency])
           })
         }
-      } else if (interval > this.__month && interval <= 3 * this.__month) {
-        console.log("3 months")
-        for (let i = start_time_unix; i <= end_time_unix; i += 4 * this.__day) {
-          let value = this.get_from_date(fromCurrency, toCoin, i.toString())
-          await value.then(resp => {
-            this.__historicValues.push(Object.values(resp)[0][fromCurrency])
-          })
-        }
-      } else if (interval > 3*this.__month && interval <= this.__year) {
-        console.log("year")
-        for (let i = start_time_unix; i <= end_time_unix; i += this.__month) {
-          let value = this.get_from_date(fromCurrency, toCoin, i.toString())
-          await value.then(resp => {
-            this.__historicValues.push(Object.values(resp)[0][fromCurrency])
-          })
-        }
-      } else if (interval > this.__year && interval <= 3 * this.__year) {
-        console.log("3 years")
-        for (let i = start_time_unix; i <= end_time_unix; i += 2 * this.__month) {
-          let value = this.get_from_date(fromCurrency, toCoin, i.toString())
-          await value.then(resp => {
-            this.__historicValues.push(Object.values(resp)[0][fromCurrency])
-          })
-        }
-      } else if (interval > 3 * this.__year) {
-        console.log("3+ years")
-        for (let i = start_time_unix; i <= end_time_unix; i += 6 * this.__month) {
-          let value = this.get_from_date(fromCurrency, toCoin, i.toString())
-          await value.then(resp => {
-            this.__historicValues.push(Object.values(resp)[0][fromCurrency])
-          })
-        }
+      } else if (this.__month < interval && interval <= 3*this.__month){
+      for (let i = start_time_unix; i <= end_time_unix; i += 4 * this.__day) {
+        let value = this.get_from_date(fromCurrency, toCoin, i.toString())
+        await value.then(resp => {
+          this.__historicValues.push(Object.values(resp)[0][fromCurrency])
+        })
       }
-      return this.__historicValues;
+    } else if (this.__month*3 < interval && interval <= 6*this.__month) {
+      for (let i = start_time_unix; i <= end_time_unix; i += this.__week) {
+        let value = this.get_from_date(fromCurrency, toCoin, i.toString())
+        await value.then(resp => {
+          this.__historicValues.push(Object.values(resp)[0][fromCurrency])
+        })
+      } 
     } else {
-      let value = this.get_from_date(fromCurrency, toCoin, this.convert_date_to_unix(startDate).toString())
-      await value.then(resp => {
-        this.__extractedValue = Object.values(resp)[0][fromCurrency]
-      })
-      return this.__extractedValue
+      for (let i = start_time_unix; i <= end_time_unix; i += this.__month) {
+        let value = this.get_from_date(fromCurrency, toCoin, i.toString())
+        await value.then(resp => {
+          this.__historicValues.push(Object.values(resp)[0][fromCurrency])
+        })
+      }
     }
+
+  }
+    return this.__historicValues;
+
+  }
+
+  async get_daily(fromCurrency: string, toCoin: string) {
+    return lastValueFrom(this.http.get("https://min-api.cryptocompare.com/data/v2/histohour?fsym=" + toCoin + "&tsym=" + fromCurrency + "&limit=24"
+      + "&?api_key=5c70d697f3b115912447a3065e90b28a69392271de80403eb2548a0f94126295"))
   }
 
   async get_from_date(fromCurrency: string, toCoin: string, date: string) {
@@ -104,7 +147,7 @@ export class GlobalService {
 
   //Denna funktionen är inte färdig än försöker extrakta själva coinen och sen när den har funnits på exchange
   async coins() {
-   return lastValueFrom(this.http.get("https://min-api.cryptocompare.com/data/blockchain/list"
+    return lastValueFrom(this.http.get("https://min-api.cryptocompare.com/data/blockchain/list"
       + "?api_key=5c70d697f3b115912447a3065e90b28a69392271de80403eb2548a0f94126295"))
   }
 }
